730a1fef71f9fef4af6eb4152657b668ee267f0b
Ryan McKeown <ryanmckeown@mail4me.com>
2026-09-18 09:05:54 +0100

    Add some more simple functions for testing

--- src/python_skeleton/main.py
+++ src/python_skeleton/main.py
@@ -1,7 +1,20 @@
 def project_name() -> str:
+    """Just return the name of the project."""
     return "python-skeleton"
 
 
+def function_with_args(a: int, b: int) -> bool:
+    """Just an example function with two arguments to test type hint
+    documentation of both the arguments and the return."""
+    return a > b
+
+
 def main() -> None:
+    """Just an example for now.
+
+    Examples:
+        >>> main()
+        Hello from python-skeleton!
+    """
     this_name = project_name()
     print(f"Hello from {this_name}!")
