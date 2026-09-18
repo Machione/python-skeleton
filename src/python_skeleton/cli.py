462dd9a04fe8c5658e37b6d02936b6513e3fd303
Ryan McKeown <ryanmckeown@mail4me.com>
2026-09-18 08:37:47 +0100

    Add a basic CLI

---
+++ src/python_skeleton/cli.py
@@ -0,0 +1,18 @@
+import click
+
+from python_skeleton.main import main
+
+
+@click.group()
+def cli() -> None:
+    """This is just an example."""
+
+
+@cli.command()
+def hello() -> None:
+    """Greetings!"""
+    main()
+
+
+if __name__ == "__main__":
+    cli()
